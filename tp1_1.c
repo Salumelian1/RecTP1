679e686cf22353218f1c913c552c96e1a3d92a6b
Salumelian1 <salustianomeliann@gmail.com>
2025-07-19 16:17:56 -0300

    Se termino el problema 3) e

--- tp1_1.c
+++ tp1_1.c
@@ -2,4 +2,12 @@
 
 int main(){
     printf("Hola mundo");
+    int *puntero, a = 10;
+    puntero = &a;
+    printf("\nContenido del puntero %d", *puntero);
+    printf("\nDireccion de memoria almacenada por el puntero: %p",puntero);
+    printf("\nDireccion de memoria de la variable: %p",&a);
+    printf("\nDireccion de memoria del puntero: %p",&puntero); 
+    printf("\nEl tamaño de 'a' es de: %d bits",sizeof(a));  
+    return 0;
 }
